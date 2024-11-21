5bcde8abe8bbaca1f0c07d6f3c2a2b9b92b857f5
matejav <matejav@stud.ntnu.no>
2024-11-21 13:18:48 +0100

    Added a utils files which will be responsible for exception handling. will probably also contain code for input-validation

---
+++ src/main/java/edu/ntnu/idi/idatt/Utils/Utils.java
@@ -0,0 +1,42 @@
+package edu.ntnu.idi.idatt.Utils;
+
+import java.time.LocalDate;
+
+public class Utils {
+  ////
+  public static void validateName(String name) {
+    if (name == null || name.trim().isEmpty()) {
+      throw new IllegalArgumentException("Unit can NOT be a null or empty value");
+    }
+  }
+
+  public static void validatePrice(double pricePerUnit) {
+    if (pricePerUnit <= 0) {
+      throw new IllegalArgumentException("Price can NOT be a negative value");
+    }
+  }
+
+  public static void validateAmount(double amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Amount must be greater than 0.");
+    }
+  }
+
+  public static void validateUnit(String unit) {
+    if (unit == null || unit.trim().isEmpty()) {
+      throw new IllegalArgumentException("Unit cannot be null or empty.");
+    }
+  }
+
+  public static void validateExpiryDate(LocalDate expiryDate) {
+    if (expiryDate == null) {
+      throw new IllegalArgumentException("Expiry date cannot be null.");
+    }
+    if (expiryDate.isBefore(LocalDate.now())) {
+      throw new IllegalArgumentException("Expiry date cannot be before the current date.");
+    }
+  }
+
+}
+
+
