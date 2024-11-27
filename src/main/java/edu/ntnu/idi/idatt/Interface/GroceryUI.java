7daa78566990e667cc4caa9eaa9157af29e22963
matejav <matejav@stud.ntnu.no>
2024-11-27 13:16:43 +0100

    Added to new cases to the GroceryUI. now user will have the option to view total value of expired groceries, and also list all expired groceries

--- src/main/java/edu/ntnu/idi/idatt/Interface/GroceryUI.java
+++ src/main/java/edu/ntnu/idi/idatt/Interface/GroceryUI.java
@@ -1,7 +1,7 @@
 package edu.ntnu.idi.idatt.Interface;
 
-import edu.ntnu.idi.idatt.FoodStorage;
-import edu.ntnu.idi.idatt.Grocery;
+import edu.ntnu.idi.idatt.Model.FoodStorage;
+import edu.ntnu.idi.idatt.Model.Grocery;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
@@ -17,11 +17,14 @@ public class GroceryUI {
   private static final int FIND_GROCERY_IN_STORAGE = 4;
   private static final int BEST_BEFORE_EXPIRY_DATE = 5;
   private static final int TOTAL_VALUE_OF_GROCERIES_IN_STORAGE = 6;
-  private static final int LIST_ALL_GROCERIES = 7;
+  private static final int VIEW_EXPIRED_GROCERIES = 7;
+  private static final int TOTAL_VALUE_OF_EXPIRED_GROCERIES = 8;
+  private static final int LIST_ALL_GROCERIES = 9;
   private static final int EXIT = 0;
 
   private final Scanner scanner = new Scanner(System.in);
-  private final FoodStorage storage = new FoodStorage();
+  private FoodStorage storage = null;
+
 
   /**
    * Presents the menu for the user and awaits input from the user.
@@ -38,7 +41,9 @@ public class GroceryUI {
     System.out.println("4. Find grocery in storage");
     System.out.println("5. Return groceries with best-before date before a given expiry date");
     System.out.println("6. return total value of ALL groceries in storage");
-    System.out.println("7. List all groceries");
+    System.out.println("7. View all expired groceries");
+    System.out.println("8. return total value of ALL EXPIRED groceries in storage");
+    System.out.println("9. List all groceries");
     System.out.println("0. Quit");
     System.out.println("\nPlease enter a number between 1 and 9.\n");
 
@@ -46,7 +51,7 @@ public class GroceryUI {
     try {
       menuChoice = Integer.parseInt(scanner.nextLine().trim());
     } catch (NumberFormatException e) {
-      System.out.println("Invalid input. Please enter a number.");
+      System.out.println("Invalid input. Please enter a following numebr (1-9).");
     }
 
     return menuChoice;
@@ -63,6 +68,11 @@ public class GroceryUI {
       switch (menuChoice) {
         case ADD_GROCERY:
           //TODO
+          try {
+            //Grocery new_crocegery = new Grocery("pass");
+          } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+          }
 
         case REMOVE_GROCERY:
           //TODO
@@ -79,6 +89,12 @@ public class GroceryUI {
         case TOTAL_VALUE_OF_GROCERIES_IN_STORAGE:
           //TODO
 
+        case VIEW_EXPIRED_GROCERIES:
+          //TODO
+
+        case TOTAL_VALUE_OF_EXPIRED_GROCERIES:
+          //TODO
+
         case LIST_ALL_GROCERIES:
           System.out.println(storage);
 
@@ -89,61 +105,49 @@ public class GroceryUI {
   }
 
   public void init() {
+    storage = new FoodStorage();
+
     //DUMMY VALUES----------------
-    Grocery milk1 = new Grocery("Milk", 15.0, 1.0, "liters", LocalDate.of(2023, 11, 20));
-    Grocery milk2 = new Grocery("Milk", 15.0, 2.0, "liters", LocalDate.of(2023, 12, 1));
-    Grocery milk3 = new Grocery("Milk", 15.0, 1.5, "liters", LocalDate.of(2023, 12, 15));
-
-    Grocery bread1 = new Grocery("Bread", 25.0, 1.0, "pieces", LocalDate.of(2023, 11, 18));
-    Grocery bread2 = new Grocery("Bread", 25.0, 2.0, "pieces", LocalDate.of(2023, 11, 22));
-    Grocery bread3 = new Grocery("Bread", 25.0, 1.0, "pieces", LocalDate.of(2023, 11, 25));
-
-    Grocery eggs1 = new Grocery("Eggs", 5.0, 12.0, "pieces", LocalDate.of(2023, 12, 15));
-    Grocery eggs2 = new Grocery("Eggs", 5.0, 6.0, "pieces", LocalDate.of(2023, 12, 5));
-
-    Grocery chicken1 = new Grocery("Chicken", 150.0, 1.0, "kg", LocalDate.of(2023, 11, 25));
-    Grocery chicken2 = new Grocery("Chicken", 150.0, 2.0, "kg", LocalDate.of(2023, 12, 10));
-
-    Grocery rice = new Grocery("Rice", 20.0, 5.0, "kg", LocalDate.of(2025, 1, 1));
-
-    Grocery apples1 = new Grocery("Apples", 30.0, 3.0, "kg", LocalDate.of(2023, 11, 28));
-    Grocery apples2 = new Grocery("Apples", 30.0, 1.5, "kg", LocalDate.of(2023, 12, 5));
-
-    Grocery bananas = new Grocery("Bananas", 20.0, 2.0, "kg", LocalDate.of(2023, 11, 22));
-
-    Grocery cheese = new Grocery("Cheese", 80.0, 1.0, "kg", LocalDate.of(2024, 3, 10));
-
-    Grocery butter = new Grocery("Butter", 50.0, 0.5, "kg", LocalDate.of(2024, 5, 20));
-
-    Grocery yogurt1 = new Grocery("Yogurt", 10.0, 0.5, "liters", LocalDate.of(2023, 12, 1));
-    Grocery yogurt2 = new Grocery("Yogurt", 10.0, 1.0, "liters", LocalDate.of(2023, 12, 10));
-
-    Grocery potatoes = new Grocery("Potatoes", 15.0, 10.0, "kg", LocalDate.of(2024, 2, 15));
-
-    Grocery carrots = new Grocery("Carrots", 12.0, 5.0, "kg", LocalDate.of(2024, 1, 5));
-
-    Grocery sugar = new Grocery("Sugar", 25.0, 2.0, "kg", LocalDate.of(2025, 6, 1));
-
-    Grocery salt = new Grocery("Salt", 10.0, 1.0, "kg", LocalDate.of(2026, 1, 1));
-
-    Grocery flour = new Grocery("Flour", 40.0, 3.0, "kg", LocalDate.of(2024, 7, 30));
-
     List<Grocery> groceryList = List.of(
-        milk1, milk2, milk3,
-        bread1, bread2, bread3,
-        eggs1, eggs2,
-        chicken1, chicken2,
-        rice,
-        apples1, apples2,
-        bananas,
-        cheese,
-        butter,
-        yogurt1, yogurt2,
-        potatoes,
-        carrots,
-        sugar,
-        salt,
-        flour
+        new Grocery("Milk", 15.0, 1.0, "l", LocalDate.of(2024, 12, 21)),
+        new Grocery("Milk", 15.0, 1.0, "l", LocalDate.of(2024, 12, 21)),
+        new Grocery("Milk", 15.0, 1.0, "l", LocalDate.of(2024, 12, 21)),
+        new Grocery("Milk", 15.0, 2.0, "l", LocalDate.of(2024, 11, 25)),
+        new Grocery("Milk", 15.0, 1.5, "l", LocalDate.of(2024, 12, 15)),
+
+        new Grocery("Bread", 25.0, 1.0, "Stk", LocalDate.of(2024, 11, 25)),
+        new Grocery("Bread", 25.0, 2.0, "Stk", LocalDate.of(2024, 11, 28)),
+        new Grocery("Bread", 25.0, 1.0, "Stk", LocalDate.of(2024, 12, 1)),
+
+        new Grocery("Eggs", 5.0, 12.0, "Stk", LocalDate.of(2024, 12, 15)),
+        new Grocery("Eggs", 5.0, 6.0, "Stk", LocalDate.of(2024, 12, 5)),
+
+        new Grocery("Chicken", 150.0, 1.0, "kg", LocalDate.of(2024, 11, 30)),
+        new Grocery("Chicken", 150.0, 2.0, "kg", LocalDate.of(2024, 12, 10)),
+
+        new Grocery("Rice", 20.0, 5.0, "kg", LocalDate.of(2025, 1, 10)),
+
+        new Grocery("Apples", 30.0, 3.0, "kg", LocalDate.of(2024, 11, 28)),
+        new Grocery("Apples", 30.0, 1.5, "kg", LocalDate.of(2024, 12, 5)),
+
+        new Grocery("Bananas", 20.0, 2.0, "kg", LocalDate.of(2024, 11, 30)),
+
+        new Grocery("Cheese", 80.0, 1.0, "kg", LocalDate.of(2025, 3, 10)),
+
+        new Grocery("Butter", 50.0, 0.5, "kg", LocalDate.of(2025, 5, 20)),
+
+        new Grocery("Yogurt", 10.0, 0.5, "l", LocalDate.of(2024, 12, 1)),
+        new Grocery("Yogurt", 10.0, 1.0, "l", LocalDate.of(2024, 12, 10)),
+
+        new Grocery("Potatoes", 15.0, 10.0, "kg", LocalDate.of(2025, 2, 15)),
+
+        new Grocery("Carrots", 12.0, 5.0, "kg", LocalDate.of(2025, 1, 5)),
+
+        new Grocery("Sugar", 25.0, 2.0, "kg", LocalDate.of(2025, 6, 1)),
+
+        new Grocery("Salt", 10.0, 1.0, "kg", LocalDate.of(2026, 1, 1)),
+
+        new Grocery("Flour", 40.0, 3.0, "kg", LocalDate.of(2025, 7, 30))
     );
 
     for (Grocery g : groceryList) {
