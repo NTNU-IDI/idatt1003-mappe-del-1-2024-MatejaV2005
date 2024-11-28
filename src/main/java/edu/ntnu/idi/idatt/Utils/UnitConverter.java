62821639c58951c8c360f501cb029bc222492b07
matejav <matejav@stud.ntnu.no>
2024-11-28 14:18:08 +0100

    switched default amount for dry-ingredients to grams instead of kg

--- src/main/java/edu/ntnu/idi/idatt/Utils/UnitConverter.java
+++ src/main/java/edu/ntnu/idi/idatt/Utils/UnitConverter.java
@@ -2,15 +2,15 @@ package edu.ntnu.idi.idatt.Utils;
 
 public class UnitConverter {
 
-  public static double convertToStandardUnit(double amount, String unit) {
+  public static double ConvertUnitAmount(double amount, String unit) {
     switch (unit.toLowerCase()) {
       case "ml": return amount / 1000;
       case "dl": return amount / 10;
-      case "mg": return amount / 1_000_000;
-      case "g": return amount / 1000;
-      case "l":
-      case "stk":
-      case "kg": return amount;
+      case "mg": return amount / 1000;
+      case "kg": return amount * 1000;
+      case "g": return amount;
+      case "l": return amount;
+      case "stk": return amount;
       default: throw new IllegalArgumentException("Unsupported unit: " + unit);
     }
   }
